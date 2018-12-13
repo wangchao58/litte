babde5093276ccc9e48fb0d5b291d83a9c44e652
kaixuan1314 <kaixuan.jiang@foxmail.com>
2018-12-13 14:01:25 +0800

    招聘相关联系信息

--- src/main/java/com/litte/controller/jurisdiction/TrecruitInfoController.java
+++ src/main/java/com/litte/controller/jurisdiction/TrecruitInfoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/trecruitInfo")
+@RequestMapping(value = "/trecruitInfo")
 public class TrecruitInfoController extends BaseController {
     @Autowired
     TrecruitInfoService trecruitInfoService;
